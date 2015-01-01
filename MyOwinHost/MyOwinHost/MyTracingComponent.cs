1b28ecd68a43745cb5079e3d9f2aecc09f379a9b
Federico Rota <federico.rota01@gmail.com>
2015-01-01 02:17:00 +0100

    OwinHost.exe example

---
+++ MyOwinHost/MyOwinHost/MyTracingComponent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyOwinHost
+{
+    public class MyTracingComponent
+    {
+            private readonly Func<IDictionary<string, object>, System.Threading.Tasks.Task> next;
+            private readonly int _requestCounter;
+
+        // Additional parameters available after next...
+        public MyTracingComponent(Func<IDictionary<string, object>, System.Threading.Tasks.Task> next)
+            {
+                this.next = next;
+            }
+
+        public System.Threading.Tasks.Task Invoke(IDictionary<string, object> env)
+            {
+                var requestPath = (string)env["owin.RequestPath"];
+
+                // ...here do something meaningful. Like tracing...
+                if(requestPath == "/")
+                    env.Add("myDemo.MyTracingComponent", "Hello from tracing component! Try some request different than root!");
+
+                // We must pass-through onto the next module
+                return this.next(env);
+            }
+    }
+}
