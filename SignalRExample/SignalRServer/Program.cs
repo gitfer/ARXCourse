02b1931c024eae46ef5a787b301b8c463bdc89cf
Federico Rota <federico.rota01@gmail.com>
2015-01-01 02:41:40 +0100

    SignalR example Owin selfhosted

---
+++ SignalRExample/SignalRServer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace SignalRServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Based on: http://www.asp.net/signalr/overview/deployment/tutorial-signalr-self-host
+            // Pacchetti per server: SignalR.selfhost + Microsoft.Owin.CORS
+            // Pacchetti per client: AspNet.SignalR.JS
+
+            // This will *ONLY* bind to localhost, if you want to bind to all addresses
+            // use http://*:8080 to bind to all addresses. 
+            // See http://msdn.microsoft.com/en-us/library/system.net.httplistener.aspx 
+            // for more information.
+            string url = "http://localhost:8080";
+            using (WebApp.Start(url))
+            {
+                Console.WriteLine("Server running on {0}", url);
+                Console.ReadLine();
+            }
+        }
+    }
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+    public class MyHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addMessage(name, message);
+        }
+    }
+}
